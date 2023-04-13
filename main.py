c1f94c31bed511aab3f6bdc005288f344b0ef9d5
Aaron Francis <aaronfrancis99@gmail.com>
2023-04-12 22:38:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """This version will use standard API loading method"""
 import requests
 
+symbol = input('What stock would you like to get data for?\n')
 
-url = "fhttps://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&apikey=0K0L52MI81VV967R"
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&apikey=0K0L52MI81VV967R"
 r = requests.get(url)
 data = r.json()
 
-# print(data)
+print(data)
 
-symbol = input('What stock would you like to get data for? /n')
